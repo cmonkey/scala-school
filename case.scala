0a0dd5d60e986eefc99b45bd0e95fde9e6ab25fd
CMonkey <42.codemonkey@gmail.com>
2016-10-31 14:47:11 +0800

    case

---
+++ case.scala
@@ -0,0 +1,8 @@
+def describe(x: Any) = x match {
+  case 5 => "five"
+  case true => "truth"
+  case "hello" => "hi!"
+  case Nil => "the empty list"
+  case somthingelse => "something else " + somthingelse
+
+}
